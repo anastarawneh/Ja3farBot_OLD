5dab0a50a7051e9d29f1766bc923b2971e44844e
anastarawneh <anastarawneh2001@icloud.com>
2020-05-26 18:11:48 +0300

    Create Extensions.cs

---
+++ rJordanBot/Core/Methods/Extensions.cs
@@ -0,0 +1,25 @@
+﻿using Discord;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace rJordanBot.Core.Methods
+{
+    public static class Extensions
+    {
+        public static string CapitalizeFirst(this string input)
+        {
+            string output;
+
+            char first = input[0];
+            output = char.ToUpper(first) + input.Substring(1);
+
+            return output;
+        }
+
+        public static string GetRGB(this Color color)
+        {
+            return $"#{color.R:X2}{color.G:X2}{color.B:X2}";
+        }
+    }
+}
