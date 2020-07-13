b8518136934a42ee447f42522c28030df7be4ee0
anastarawneh <anastarawneh2001@icloud.com>
2020-07-13 18:21:51 +0300

    Implement player list command

--- rJordanBot/Core/Commands/Minecraft.cs
+++ rJordanBot/Core/Commands/Minecraft.cs
@@ -12,5 +12,19 @@ namespace rJordanBot.Core.Commands
 {
     public class Minecraft : InteractiveBase
     {
+        [Command("list")]
+        [MinecraftCommand]
+        public async Task List()
+        {
+            getServerStatus.apiData.apiPlayer[] players = Multicraft.GetServerStatus().Data.Players;
+            if (players.Count() == 0) await ReplyAsync("There are 0 players online.");
+            else if (players.Count() == 1) await ReplyAsync($"There is 1 player online:\n- {players.First().Name}");
+            else
+            {
+                string response = $"There are {players.Count()} players online:";
+                foreach (var player in players) response += $"\n- {player.Name}";
+                await ReplyAsync(response);
+            }
+        }
     }
 }
